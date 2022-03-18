f8a724166fb6e6be42f8b223914724070f452260
Ian <new@news-MacBook-Pro.local>
2022-03-18 10:58:54 -0600

    update to productList.jsx

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,19 +1,23 @@
-// import { Filter } from '@material-ui/icons';
 import { Add, Remove } from "@material-ui/icons";
-import React from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import { mobile } from "../responsive";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { publicRequest } from "../requestMethods";
+import { addProduct } from "../redux/cartRedux";
+import { useDispatch } from "react-redux";
+
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
-  ${mobile({ flexDirection: "column", padding:"10px" })}
+  ${mobile({ padding: "10px", flexDirection: "column" })}
 `;
 
 const ImgContainer = styled.div`
@@ -23,6 +27,7 @@ const ImgContainer = styled.div`
 const Image = styled.img`
   width: 100%;
   height: 90vh;
+  object-fit: cover;
   ${mobile({ height: "40vh" })}
 `;
 
@@ -33,23 +38,23 @@ const InfoContainer = styled.div`
 `;
 
 const Title = styled.h1`
-  font-weight: 900;
+  font-weight: 200;
 `;
 
 const Desc = styled.p`
   margin: 20px 0px;
-  font-size: 20px;
 `;
 
 const Price = styled.span`
   font-weight: 100;
-  font-size: 30px;
+  font-size: 40px;
 `;
 
 const FilterContainer = styled.div`
   width: 50%;
-  margin: 25px -15px;
+  margin: 30px 0px;
   display: flex;
+  justify-content: space-between;
   ${mobile({ width: "100%" })}
 `;
 
@@ -58,94 +63,140 @@ const Filter = styled.div`
   align-items: center;
 `;
 
-const FilterQuantity = styled.select`
-  margin-left: 15px;
-  font-size: 20px;
-`;
-
 const FilterTitle = styled.span`
-  font-size: 25px;
-  padding: 20px;
+  font-size: 20px;
+  font-weight: 200;
 `;
 
-const FilterQuantityOption = styled.option`
-font-size: 30px;
+const FilterRoast = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
 `;
+
+const FilterRegion = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
 
 const AddContainer = styled.div`
-width: 40%;
-display: flex;
-align-items: center;
-justify-content: space-between;
-${mobile({ width: "100%" })}
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })}
 `;
 
 const AmountContainer = styled.div`
-display: flex;
-align-items: center;
-font-weight: 700;
+  display: flex;
+  align-items: center;
+  font-weight: 700;
 `;
+
 const Amount = styled.span`
-width: 30px;
-height: 30px;
-border-radius: 10px;
-border: 1px solid teal;
-display: flex;
-align-items: center;
-justify-content: center;
-margin: 0px 5px;
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
 `;
 
 const Button = styled.button`
-padding:  15px;
-border: 2px solid teal;
-background-color: white;
-cursor: pointer;
-font-weight: 500;
-
-&:hover {
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover {
     background-color: #f8f4f4;
-}
+  }
 `;
 
 
 const Product = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [roast, setRoast] = useState("");
+  const [region, setRegion] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await publicRequest.get("/products/find/" + id);
+        setProduct(res.data);
+      } catch {}
+    };
+    getProduct();
+  }, [id]);
+
+  const handleQuantity = (type) => {
+    if (type === "dec") {
+      quantity > 1 && setQuantity(quantity - 1);
+    } else {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const handleClick = () => {
+    dispatch(
+      addProduct({ ...product, quantity, roast, region })
+    );
+  };
+
+
   return (
     <Container>
-      <Navbar />
-      <Announcement />
-      <Wrapper>
-        <ImgContainer>
-          <Image src="https://cdn.shopify.com/s/files/1/0087/6606/2639/products/image_2_20_f5fb74c1-fc47-4f1b-8bea-166d3a5d8efb_550x825.png?v=1642534126" />
-        </ImgContainer>
-        <InfoContainer>
-          <Title>RWANDA IBISI MOUNTAIN</Title>
-          <Desc>molasses | cranberry | blackcurrant</Desc>
-          <Price>$12/8oz ** $18/12oz ** $23/16oz</Price>
-          <FilterContainer>
-            <Filter>
-              <FilterTitle>Quantity</FilterTitle>
-              <FilterQuantity>
-                <FilterQuantityOption>8oz</FilterQuantityOption>
-                <FilterQuantityOption>12oz</FilterQuantityOption>
-                <FilterQuantityOption>16oz</FilterQuantityOption>
-              </FilterQuantity>
-            </Filter>
-          </FilterContainer>
-          <AddContainer>
-              <AmountContainer>
-                  <Remove/>
-                  <Amount>1</Amount>
-                  <Add/>
-              </AmountContainer>
-              <Button>ADD TO CART</Button>
-          </AddContainer>
-        </InfoContainer>
-      </Wrapper>
-      <Newsletter />
-      <Footer />
-    </Container>
-  );
+    <Navbar />
+    <Announcement />
+    <Wrapper>
+      <ImgContainer>
+        <Image src={product.img} />
+      </ImgContainer>
+      <InfoContainer>
+        <Title>{product.title}</Title>
+        <Desc>{product.desc}</Desc>
+        <Price>$ {product.price}</Price>
+        <FilterContainer>
+          <Filter>
+            <FilterTitle>Roast</FilterTitle>
+            {product.roast?.map((c) => (
+              <FilterRoast roast={c} key={c} onClick={() => setRoast(c)} />
+            ))}
+          </Filter>
+          <Filter>
+            <FilterTitle>Region</FilterTitle>
+            <FilterRegion onChange={(e) => setRegion(e.target.value)}>
+              {product.region?.map((s) => (
+                <FilterSizeOption key={s}>{s}</FilterSizeOption>
+              ))}
+            </FilterRegion>
+          </Filter>
+        </FilterContainer>
+        <AddContainer>
+          <AmountContainer>
+            <Remove onClick={() => handleQuantity("dec")} />
+            <Amount>{quantity}</Amount>
+            <Add onClick={() => handleQuantity("inc")} />
+          </AmountContainer>
+          <Button onClick={handleClick}>ADD TO CART</Button>
+        </AddContainer>
+      </InfoContainer>
+    </Wrapper>
+    <Newsletter />
+    <Footer />
+  </Container>
+);
 };
 
 export default Product;
